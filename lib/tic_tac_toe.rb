0ffa3421cc9cb0ef9ae88818622dd3ad823aae31
Tommy Jake <tomlom1984@hotmail.co.uk>
2019-08-01 22:35:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,17 +1,18 @@
 class TicTacToe
+
   def initialize (board = Array.new(9, " "))
    @board = board
   end
-
   def board=(board)
     @board
   end
-
   def board=(board)
     @board = board
   end
-
   def board
       @board
   end
+
+
+
 end
